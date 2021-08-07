4f5e504404d3969fdc1a3bbb8a10ae3d367356d7
AK6263 <abhaykshirsagar01@gmail.com>
2021-08-08 00:09:33 +0530

    Checking Trade History

---
+++ trades.py
@@ -0,0 +1,39 @@
+import hmac
+import hashlib
+import base64
+import json
+import time
+import requests
+
+# Enter your API Key and Secret here. If you don't have one, you can generate it from the website.
+coindcx = json.load(open('../coindcx-api/api.json'))
+
+key = coindcx['apikey']
+secret = coindcx['secretkey']
+
+# python3
+secret_bytes = bytes(secret, encoding='utf-8')
+
+
+# Generating a timestamp.
+timeStamp = int(round(time.time() * 1000))
+
+body = {
+  "timestamp": timeStamp
+}
+
+json_body = json.dumps(body, separators = (',', ':'))
+
+signature = hmac.new(secret_bytes, json_body.encode(), hashlib.sha256).hexdigest()
+
+url = "https://api.coindcx.com/exchange/v1/orders/trade_history"
+
+headers = {
+    'Content-Type': 'application/json',
+    'X-AUTH-APIKEY': key,
+    'X-AUTH-SIGNATURE': signature
+}
+
+response = requests.post(url, data = json_body, headers = headers)
+data = response.json()
+print(data)
